4669297f431d1e51a68bdab2bc9175fe6bbd477f
jwilliams722 <jwilliams722@gmail.com>
2013-08-02 19:30:38 -0600

    wip

--- palindrome_recursion/lib/palindrome.rb
+++ palindrome_recursion/lib/palindrome.rb
@@ -3,8 +3,7 @@ class Palindrome
   def palindrome?(string)
     #string.gsub!(" ,", "") only takes care of space and comma while the other takes care of any non-letter characters
     string = string.gsub(/\W/,'').downcase
-    return false if string == ""
-    return true if string.length == 1
+    return true if string == "" || string.length == 1
     letters = string.split("")
     first_letter = letters.shift
     last_letter = letters.pop
--- palindrome_recursion/spec/lib/palindrome_spec.rb
+++ palindrome_recursion/spec/lib/palindrome_spec.rb
@@ -2,29 +2,81 @@ require "./lib/palindrome"
 
 describe Palindrome do
   describe "#palindrome?(string)" do
-    it "should return false for empty string" do
-      expect(subject.palindrome?("")).to be_false
+    it "should return true for empty string" do
+      expect(subject.palindrome?("")).to be_true
     end
     it "should return true for string with one letter" do
       expect(subject.palindrome?("a")).to be_true
     end
-    it "should return true for aa" do
-      expect(subject.palindrome?("aa")).to be_true
+
+    describe "two letter strings" do
+      it "should return true for aa" do
+        expect(subject.palindrome?("aa")).to be_true
+      end
+
+      it "should return false for ab" do
+        expect(subject.palindrome?("ab")).to be_false
+      end
     end
-    it "should return true for aba" do
-      expect(subject.palindrome?("aba")).to be_true
+
+    describe "three letter strings" do
+      it "should return true for aba" do
+       expect(subject.palindrome?("aba")).to be_true
+      end
+
+      it "should return false for abc" do
+        expect(subject.palindrome?("abc")).to be_false
+      end
     end
-    it "should return true for radar" do
-      expect(subject.palindrome?("radar")).to be_true
+
+    describe "common palindrome strings" do
+      it "should return true for dad" do
+        expect(subject.palindrome?("dad")).to be_true
+      end
+
+      it "should return true for radar" do
+        expect(subject.palindrome?("radar")).to be_true
+      end
+
+      it "should return true for racecar" do
+        expect(subject.palindrome?("racecar")).to be_true
+      end
+
+      it "should return false for doughnut" do
+        expect(subject.palindrome?("doughnut")).to be_false
+      end
     end
-    it "should return false for doughnut" do
-      expect(subject.palindrome?("doughnut")).to be_false
+
+    describe "it handles symbols and white space" do
+      it "should return false for $%!@#" do
+        expect(subject.palindrome?("$%!@#")).to be_true
+      end
+
+      it "should know about a man, a plan, a canal, PANAMA!" do
+        expect(subject.palindrome?("A man, a plan, a canal, PANAMA!")).to be_true
+      end
     end
-    it "should know about PANAMA!" do
-      expect(subject.palindrome?("A man, a plan, a canal, PANAMA!")).to be_true
-    end
-    it "should return false for $%!@#" do
-      expect(subject.palindrome?("$%!@#")).to be_false
+
+    describe "it handles numbers" do
+      it "should return false for 1" do
+        expect(subject.palindrome?("1")).to be_true
+      end
+
+      it "should return true for 11" do
+        expect(subject.palindrome?("11")).to be_true
+      end
+
+      it "should return false for 12" do
+        expect(subject.palindrome?("12")).to be_false
+      end
+
+      it "should return true for 121" do
+        expect(subject.palindrome?("121")).to be_true
+      end
+
+      it "should return false for 123" do
+        expect(subject.palindrome?("123")).to be_false
+      end
     end
   end
 end
